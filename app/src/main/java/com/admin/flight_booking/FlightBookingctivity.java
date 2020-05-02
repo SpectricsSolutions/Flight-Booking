040dafce4fa6d72a47ae723045c61684ee37501f
Spectrics Solutions <spectricssolutions@gmail.com>
2020-05-02 15:26:39 +0530

    Code Changes

---
+++ app/src/main/java/com/admin/flight_booking/FlightBookingctivity.java
@@ -0,0 +1,14 @@
+package com.admin.flight_booking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlightBookingctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_bookingctivity);
+    }
+}
---
+++ app/src/main/java/com/admin/flight_booking/Search_FlightActivity.java
@@ -0,0 +1,14 @@
+package com.admin.flight_booking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Search_FlightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search__flight);
+    }
+}
